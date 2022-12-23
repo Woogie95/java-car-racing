5580fdc995412a5a422796a35179a768050d0cac
Woogie95 <choisw950928@gmail.com>
2022-12-23 13:59:25 +0900

    feat : Position 객체 생성

--- src/carracing/Position.java
+++ src/carracing/Position.java
@@ -2,18 +2,25 @@ package carracing;
 
 public class Position {
 
+    private static final int ADD_POSITION = 1;
+    private static final int POSITION_INITIAL_VALUE = 0;
+    private static final int MIN_MOVE_POSSIBLE_VALUE = 4;
+
     private final int position;
+
+    public Position() {
+        this.position = POSITION_INITIAL_VALUE;
+    }
 
     public Position(int position) {
         this.position = position;
     }
 
-    public void addPosition() {
-
-    }
-
-    public int getPosition() {
-        return position;
+    public Position MovePosition(int randomNumber) {
+        if (randomNumber >= MIN_MOVE_POSSIBLE_VALUE) {
+            return new Position(this.position + ADD_POSITION);
+        }
+        return new Position();
     }
 
 }
